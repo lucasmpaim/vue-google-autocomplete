19e0549552e3bd9f398ca109a55914313dabcda0
Lucas Mrowskovsky Paim <lucasmpaim1@gmail.com>
2019-02-19 09:52:47 -0300

    add short_country to address_data

--- src/VueGoogleAutocomplete.vue
+++ src/VueGoogleAutocomplete.vue
@@ -23,7 +23,8 @@
         administrative_area_level_1: 'short_name',
         administrative_area_level_2: 'long_name',
         country: 'long_name',
-        postal_code: 'short_name'
+        postal_code: 'short_name',
+	short_country: 'short_name'
     };
 
     const CITIES_TYPE = ['locality', 'administrative_area_level_3'];
